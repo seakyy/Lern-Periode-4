5fc0b6876f215f17fa52897e0bef44988b7547d2
David <142885534+seakyy@users.noreply.github.com>
2024-03-05 11:17:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Gleggmire_Discord_Bot.config;
+using System.Threading.Tasks;
+
+namespace Gleggmire_Discord_Bot
+{
+    internal class Program
+    {
+        
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordconfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All, 
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordconfig);
+
+            Client.Ready += Client_Ready;
+            
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
